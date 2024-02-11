cf62b94e02ce3166ff82dca63a1fb330d9d6fff3
yusufiii000 <158528545+yusufiii000@users.noreply.github.com>
2024-02-11 16:22:30 +0300

    hack.sh

---
+++ hack.sh
@@ -0,0 +1,28 @@
+#script BY Yusufi Hacks Turkish is Strongs
+
+check(){
+  if https://github.com/yusufiii000/Hacking-Turkish-YusufYap-mssss.git>dev/null; then
+  cd $HOME/yusufihack/indir
+  echo 'hack' > indir.txt
+  else
+  echo " "
+  fi
+}
+check
+option() {
+  if  [-f "$HOME/yusufihack/indir/indir.txt" ];then
+  cd $HOME/yusufihack/indir
+  bash update.sh
+  else
+  echo " "
+  fi
+}
+option
+check1() {
+  if https://github.com/yusufiii000/Hacking-Turkish-YusufYap-mssss.git>dev/null; then
+  cd $HOME/yusufihack/indir
+  echo "hackk" > yama.txt
+  else
+  echo
+  fi
+}
